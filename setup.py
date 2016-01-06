36e327dc11da83f23da8aecaae6a2f37f9b1a019
Max Harlow <maxharlow@gmail.com>
2016-01-06 17:35:43 +0000

    Updated metadata

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 
 setup(
     name='csvbar',
-    version='1.3',
+    version='1.4',
     description='Draw bar graphs from CSV files in the terminal.',
     long_description=open('README.md').read(),
     author='Max Harlow',
-    author_email='maxharlow@tbij.com',
-    url='https://github.com/tbij/csvbar',
+    author_email='maxharlow@gmail.com',
+    url='https://github.com/maxharlow/csvbar',
     license='Apache',
-    py_modules=['csvbar'],
+    packages=[''],
     entry_points = {
         'console_scripts': [
             'csvbar = csvbar:main'
@@ -25,8 +25,6 @@ setup(
         'Natural Language :: English',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Topic :: Scientific/Engineering :: Information Analysis',
         'Topic :: Utilities'
