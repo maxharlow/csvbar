aff93c1361155db9a986d3bfa1b7695c44bd5129
Max Harlow <maxharlow@gmail.com>
2016-01-06 17:40:04 +0000

    Better exception handling

--- csvbar.py
+++ csvbar.py
@@ -1,38 +1,40 @@
 # coding: utf-8
 
-# csvbar -- draw bar graphs from CSV files in the terminal
-# https://github.com/tbij/csvbar
-
 import argparse
 import locale
 import csv
 import sys
 import os
 
-locale.setlocale(locale.LC_ALL, '')
-
 def main():
-    args = arguments()
-    labels, data = read(args['FILE'])
-    display(labels, data, args['width'])
+    locale.setlocale(locale.LC_ALL, '')
+    try:
+        args = arguments()
+        labels, data = read(args['FILE'])
+        display(labels, data, args['width'])
+    except BaseException as e: sys.exit(e)
 
 def arguments():
     parser = argparse.ArgumentParser(description='draw bar graphs from CSV files in the terminal')
     parser.add_argument('FILE', nargs='?', default='-', help='the CSV file to operate on -- if omitted, will accept input on STDIN')
-    parser.add_argument('--width', type=int, default=50, help='width of graph in characters (default is 50)')
-    return vars(parser.parse_args())
+    parser.add_argument('-w', '--width', type=int, default=50, help='width of graph in characters (default is 50)')
+    args = vars(parser.parse_args())
+    if args['FILE'] == '-' and sys.stdin.isatty():
+        parser.print_help(sys.stderr)
+        parser.exit(1)
+    return args
 
 def read(filename):
     labels = []
     data = []
-    if not os.path.isfile(filename) and filename != '-': sys.exit(filename + ': no such file')
+    if not os.path.isfile(filename) and filename != '-': raise Exception(filename + ': no such file')
     file = sys.stdin if filename == '-' else open(filename, 'rb')
     reader = csv.reader(file)
     headers = next(reader) # skip these
     for line in reader:
         labels.append(line[0])
         try: data.append(float(line[1]))
-        except ValueError: sys.exit(line[1] + ' is not a number')
+        except ValueError: raise Exception(line[1] + ' is not a number')
     file.close()
     return labels, data
 
